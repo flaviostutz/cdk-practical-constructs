87fdd017bf6483a5d3eb0340d08ba83fbc435290
Flavio Stutz <flaviostutz@gmail.com>
2024-01-21 03:29:47 +0100

    feat: project reorganization to make examples exercise lib usage as external so it gets bugs with lambda bundlings

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 SHELL := /bin/bash
 
 %:
-	@echo 'Running /lib:$@...'
-	@cd lib; make $@
-
 	@echo ''
-	@echo 'Running /examples:$@...'
+	@echo '>>> Running /lib:$@...'
+	@cd lib; make $@
+	@echo ''
+
+	@# Building /examples is important as it simulates the usage of the lib as external
+	@# so that is get problems with Lambda bundling, which is sensitive when distributing libs
+	@echo ''
+	@echo '>>> Running /examples:$@...'
+	@echo ''
 	@cd examples; make $@
 
 publish:
--- examples/src/cdk/stack.ts
+++ examples/src/cdk/stack.ts
@@ -1,9 +1,9 @@
 /* eslint-disable camelcase */
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import { BaseNodeJsFunction, EventType } from 'cdk-practical-constructs';
 
 import { TestConfig } from './configs';
+import { addWso2Api as addWso2PetstoreApi } from './petstore-wso2-api';
 
 export type StageStackProps = StackProps & {
   stageConfig: TestConfig;
@@ -13,12 +13,6 @@ export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props: StageStackProps) {
     super(scope, id, props);
 
-    // eslint-disable-next-line no-new
-    new BaseNodeJsFunction(this, 'test lambda', {
-      ...props.stageConfig.lambda,
-      stage: props.stageConfig.stage ?? 'dev',
-      eventType: EventType.Http,
-      description: 'Test http lambda',
-    });
+    addWso2PetstoreApi(this);
   }
 }
