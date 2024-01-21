512ef387b06a1756e5e241735671b4beee302b31
Flavio Stutz <flavio.stutz@nn.nl>
2024-01-21 03:29:47 +0100

    feat: project reorganization to make examples exercise lib usage as external so it gets bugs with lambda bundlings

---
+++ examples/src/cdk/petstore-wso2-api.ts
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+import { Construct } from 'constructs';
+import { Wso2Api, Wso2ApiProps } from 'cdk-practical-constructs';
+
+import { petstoreOpenapi } from './petstore-openapi';
+
+export const addWso2Api = (scope: Construct): void => {
+  // prepare api definition
+  const wso2Props: Wso2ApiProps = {
+    wso2Config: {
+      baseApiUrl: 'http://mywso2.com',
+      credentialsSecretManagerPath: 'arn::creds',
+    },
+    apiDefinition: {
+      wso2Version: 'v1',
+      type: 'HTTP',
+      endpointConfig: {
+        production_endpoints: {
+          url: 'http://serverabc.com',
+        },
+        endpoint_type: 'http',
+      },
+      context: '/petstore',
+      name: 'petstore-sample',
+      gatewayEnvironments: ['public'],
+      corsConfiguration: {
+        accessControlAllowOrigins: ['testwebsite.com'],
+      },
+    },
+    openapiDocument: petstoreOpenapi,
+  };
+
+  // eslint-disable-next-line no-new
+  new Wso2Api(scope, 'wso2-petstore', wso2Props);
+};
